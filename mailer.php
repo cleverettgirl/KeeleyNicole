c9650fb5bc6ad37d2f4077d6910ebc434a9fc85c
Derek Kelmanson <dkelmanson@gmail.com>
2016-09-17 21:42:42 +0000

    fixed ajax contact form, still needs styling

--- mailer.php
+++ mailer.php
@@ -3,10 +3,13 @@
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["your-name"]));
-        $name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["your-email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_POST["your-message"]);
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $number = strip_tags(trim($_POST["number"]));
+        $message = trim($_POST["message"]);
+
+
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
